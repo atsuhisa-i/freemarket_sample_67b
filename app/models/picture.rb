73eab5de5b4628e0b5266e60a276a23e10c647d5
taketsuna27 <taketsunacocoa@icloud.com>
2020-04-26 19:35:17 +0900

    Revert "Revert "aaaa""
    This reverts commit ef80581c0de28fdb91d42ae33ca595563bad7316.

--- app/models/picture.rb
+++
@@ -1,5 +0,0 @@
-class Picture < ApplicationRecord
-  belongs_to :item
-
-  mount_uploader :image, ImageUploader
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_074309) do
-
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "ancestry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.text "explanation", null: false
-    t.string "size", null: false
-    t.bigint "category_id", null: false
-    t.bigint "brand_id"
-    t.integer "condition", null: false
-    t.boolean "postage_payer", null: false
-    t.string "shipping_origin", null: false
-    t.integer "days_to_ship", null: false
-    t.integer "price", null: false
-    t.integer "trading_status", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["brand_id"], name: "index_items_on_brand_id"
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "pictures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_pictures_on_item_id"
-  end
+ActiveRecord::Schema.define(version: 2020_04_09_113213) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -73,8 +32,4 @@ ActiveRecord::Schema.define(version: 2020_04_19_074309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "brands"
-  add_foreign_key "items", "categories"
-  add_foreign_key "items", "users"
-  add_foreign_key "pictures", "items"
 end
