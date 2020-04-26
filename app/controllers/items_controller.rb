15a95a1948057c7efa9595ac2b878551b60f630a
taketsuna27 <taketsunacocoa@icloud.com>
2020-04-26 19:37:23 +0900

    Revert "Revert "Revert "aaaa"""
    This reverts commit 73eab5de5b4628e0b5266e60a276a23e10c647d5.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,28 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @item.pictures.build
+    @item.build_category
+    @item.build_brand
+    @categories = Category.all
   end
 
   def create
+    @item = current_user.items.build(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   end
 
   def show
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :size, :condition, :postage_payer, :shipping_origin, :days_to_ship, :price, :trading_status, pictures_attributes: [:image], category_attributes: [:name], brand_attributes: [:name]).merge(seller_id: current_user.id)
   end
 end
