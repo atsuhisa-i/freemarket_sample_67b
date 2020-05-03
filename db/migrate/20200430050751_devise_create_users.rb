3baa009631e0f94279006f82957811522fefc1a4
delphinium00 <ssp.7rush5@gamil.com>
2020-05-03 17:32:29 +0900

    カラム修正
    生年月日のカラム一つに

--- db/migrate/20200430050751_devise_create_users.rb
+++ db/migrate/20200430050751_devise_create_users.rb
@@ -11,8 +11,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string  :family_name, null: false
       t.string  :first_name_kana, null: false
       t.string  :family_name_kana, null: false
-      t.date :birth_year, null: false
-      t.date :birth_month, null: false
       t.date :birth_day, null: false
 
       ## Recoverable
