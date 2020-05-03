1a7ef7afbee2a06087bd97efa7c87e89d73d0330
delphinium00 <ssp.7rush5@gamil.com>
2020-05-03 17:33:48 +0900

    カラム修正に伴うバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one : credit_card, dependent: :destroy
-  has_one : deliver_address, dependent: :destroy
-  has_many : items, dependent: :destroy
-  has_many : comments, dependent: :destroy
-  has_many : likes, dependent: :destroy
-  has_many : buy_items
-  has_many : sell_items
-
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day,presence: true
+  validates :nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, presence: true
 end
