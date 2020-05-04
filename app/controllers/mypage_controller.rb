1f1bd28258dbf6316ba4519e9de9798cb497d4c2
Yoshida-yuki <delicious-a@ezweb.ne.jp>
2020-05-04 16:31:07 +0900

    マイページ　ビュー

---
+++ app/controllers/mypage_controller.rb
@@ -0,0 +1,13 @@
+class MypageController < ApplicationController
+  def show
+  end
+  
+  def user_profile
+  end
+
+  def Identification
+  end
+
+  def credit_card
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :registrations
   resources :logins
   resources :puroducts, onyl: [:index]
+  resources :mypage
 end
 
