7d1f42e3ef90ee76e65e64f7fa68d22324c621b0
delphinium00 <ssp.7rush5@gamil.com>
2020-05-04 18:20:04 +0900

    usersモデル、アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :credit_card, dependent: :destroy
+  has_one :deliver_address, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :buy_items, class_name: 'Transaction', :foreign_key => 'buyer_id'
+  has_many :sell_items, class_name: 'Transaction', :foreign_key => 'seller_id'
+  
   validates :nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day, presence: true
 end
