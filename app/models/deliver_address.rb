7634f799919b0adf91f042ad2016f0813a0e2901
delphinium00 <ssp.7rush5@gamil.com>
2020-05-05 18:09:43 +0900

    バリデーション設定(deliver_addressテーブル)

--- app/models/deliver_address.rb
+++ app/models/deliver_address.rb
@@ -1,3 +1,8 @@
 class DeliverAddress < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :zip_code, :prefecture, :municipality, presence: true
+
+  # 文字数制限
+  validates :zip_code, length: {in: 7..7}
 end
