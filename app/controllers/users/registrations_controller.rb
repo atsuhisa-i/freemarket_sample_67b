9e2a0a1d153991e353bb6eae3aaf43f4325b806e
delphinium00 <ssp.7rush5@gamil.com>
2020-05-06 14:31:41 +0900

    registrationsコントローラーにアクション追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,17 +22,21 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :new_deliver_address
   end
 
-  # def create
-  #   @user = User.new(sign_up_params)
-  #   unless @user.valid?
-  #     render :new and return
-  #   end
-  #   if @user.save
-  #     sign_in(:user, @user)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def complete
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.build_address(@address.attributes)
+    if @user.save
+      session["devise.regist_data"]["user"].clear
+      sign_in(:user, @user)
+    else
+      render :new
+    end
+  end
 
   
   # GET /resource/edit
@@ -59,12 +63,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-   protected
+    protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
-   end
+  def configure_sign_up_params
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day])
+  end
+
+  def address_params
+    params.require(:address).permit(:zipcode, :address)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
