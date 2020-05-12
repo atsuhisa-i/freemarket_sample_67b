ad7b876e61c30129beea5bf072d16c879ae9f496
delphinium00 <ssp.7rush5@gamil.com>
2020-05-12 12:46:26 +0900

    単体テスト追記

--- spec/models/deliver_address_spec.rb
+++ spec/models/deliver_address_spec.rb
@@ -25,6 +25,54 @@ describe DeliverAddress, type: :model do
       expect(deliver_address.errors[:first_name_kana]).to include("can't be blank")
     end
 
+    it "family_nameが全角である場合は登録できること" do
+      deliver_address = build(:deliver_address, family_name: "田中")
+      deliver_address.valid?
+      expect(deliver_address.errors[:family_name]).to include("全角で入力してください")
+    end
+
+    it "family_nameが全角でない場合は登録できないこと" do
+      deliver_address = build(:deliver_address, family_name: "tanaka")
+      deliver_address.valid?
+      expect(deliver_address.errors[:family_name]).to include("全角で入力してください")
+    end
+
+    it "first_nameが全角である場合は登録できること" do
+      deliver_address = build(:deliver_address, first_name: "太郎")
+      deliver_address.valid?
+      expect(deliver_address.errors[:first_name]).to include("全角で入力してください")
+    end
+
+    it "first_nameが全角でない場合は登録できないこと" do
+      deliver_address = build(:deliver_address, first_name: "tarou")
+      deliver_address.valid?
+      expect(deliver_address.errors[:first_name]).to include("全角で入力してください")
+    end
+
+    it "family_name_kanaが全角である場合は登録できること" do
+      deliver_address = build(:deliver_address, family_name_kana: "タナカ")
+      deliver_address.valid?
+      expect(deliver_address.errors[:family_name_kana]).to include("全角で入力してください")
+    end
+
+    it "family_name_kanaが全角でない場合は登録できないこと" do
+      deliver_address = build(:deliver_address, family_name_kana: "ﾀﾅｶ")
+      deliver_address.valid?
+      expect(deliver_address.errors[:family_name_kana]).to include("全角で入力してください")
+    end
+
+    it "first_name_kanaが全角である場合は登録できること" do
+      deliver_address = build(:deliver_address, first_name_kana: "タロウ")
+      deliver_address.valid?
+      expect(deliver_address.errors[:first_name_kana]).to include("全角で入力してください")
+    end
+
+    it "first_name_kanaが全角でない場合は登録できないこと" do
+      deliver_address = build(:deliver_address, first_name_kana: "tarou")
+      deliver_address.valid?
+      expect(deliver_address.errors[:first_name_kana]).to include("全角で入力してください")
+    end
+
     it "zip_codeがない場合は登録できないこと" do
       deliver_address = build(:deliver_address, zip_code: nil)
       deliver_address.valid?
@@ -42,5 +90,13 @@ describe DeliverAddress, type: :model do
       deliver_address.valid?
       expect(deliver_address).to be_valid
     end
+
+    it "郵便番号６桁以下は登録できないこと" do
+      deliver_address = build(:deliver_address, zip_code: "111111")
+      deliver_address.valid?
+      expect(deliver_address.errors[:zip_code]).to include("is too short (minimum is 6 characters)")
+    end
+
+    
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,10 +55,22 @@ describe User, type: :model do
       expect(user.errors[:email]).to include("can't be blank")
     end
 
+    it "emailに＠がない場合は登録できない" do
+      user = build(:user, email: "zzzzzzz")
+      user.valid?
+      expect(user.errors[:email]).to include("は不正な値です")
+    end
+
+    it "emailに＠がある場合は登録できる" do
+      user = build(:user, email: "codetest112@example.com")
+      user.valid?
+      expect(user).to be_valid
+    end
+
     it "family_name_kanaが全角カタカナの場合は登録できること" do
       user = build(:user, family_name_kana: "タナカ")
       user.valid?
-      expect(user).to be_valid
+      expect(user.errors[:family_name_kana]).to include("全角カタカナで入力してください")
     end
 
     it "family_name_kanaが全角カタカナでない場合は登録できないこと" do
@@ -67,5 +79,16 @@ describe User, type: :model do
       expect(user.errors[:family_name_kana]).to include("全角カタカナで入力してください")
     end
 
+    it "passwordフォーマット外だと登録できないこと" do
+      user = build(:user, password: "000", password_confirmation: "000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 7 characters)")
+    end
+
+    it "passwordフォーマットがただしければ登録できること" do
+      user = build(:user, password: "zxcv000", password_confirmation: "zxcv000")
+      user.valid?
+      expect(user).to be_valid
+    end
   end
 end
