eae5af69af572cacccb0a36e189043b6b5af625d
taketsuna27 <taketsunacocoa@icloud.com>
2020-05-16 17:47:24 +0900

    Update comments_controller.rb
    pathをprefixに変更
    コメントの保存に失敗した場合
    同じページにリダイレクトするように変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/items/#{comment.item.id}"  
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to item_path(@comment.item.id)
+    else
+      redirect_to item_path(@comment.item.id)
+    end
   end
   private
   def comment_params
