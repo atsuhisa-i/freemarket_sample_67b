3634c9d1a619bb20d7481fb876413bf8bd3cd318
taketsuna27 <taketsunacocoa@icloud.com>
2020-05-16 19:32:46 +0900

    Update items_controller.rb
    削除に失敗した場合トップページに戻す。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,11 +31,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
-
 private
 
   def item_params
