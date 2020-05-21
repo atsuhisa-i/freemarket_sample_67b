756713948ed35758d9bb10faf87d6fa52736ac3b
delphinium00 <ssp.7rush5@gamil.com>
2020-05-21 22:41:09 +0900

    schma更新

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2020_05_14_121836) do
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
   end
 
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
+
   create_table "days_to_ships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "days_to_ship_id"
     t.string "days"
