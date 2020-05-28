f2c387c3407e2d27b72751f99cfc49859c59cd5c
delphinium00 <ssp.7rush5@gamil.com>
2020-05-29 02:02:53 +0900

    credit_cardsコントローラー修正
    変数名、記述内容変更

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,9 +1,9 @@
 class CreditCardsController < ApplicationController
   require "payjp"
+  before_action :set_card, only: [:destroy, :show]
 
   def new
-    card = CreditCard.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    credit_card = CreditCard.where(user_id: current_user.id )
   end
 
   def create #payjpとCardのデータベース作成を実施します。
@@ -17,39 +17,35 @@ class CreditCardsController < ApplicationController
       card: params['payjp-token'],
       metadata: {user_id: current_user.id}
       )
-      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "show"
+      @credit_card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @credit_card.save
+        redirect_to users_path
       else
         redirect_to action: "create"
       end
     end
   end
 
-  def delete #PayjpとCardデータベースを削除
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      customer.delete
-      card.delete
-    end
+  def destroy #PayjpとCardデータベースを削除
+    @credit_card = current_user.credit_card
+    if @credit_card.destroy
       redirect_to action: "new"
+    end
   end
-
+ 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
+    @credit_card = CreditCard.where(user_id: current_user.id).first
+    if @credit_card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@credit_card.customer_id)
+      @default_card_information = customer.cards.retrieve(@credit_card.card_id)
     end
   end
 
   private
+
   def set_card
     @card = CreditCard.where(user_id: current_user.id).first
   end
