f2d422245dd48b3df0efce595b21b33806139ec9
delphinium00 <ssp.7rush5@gamil.com>
2020-05-29 21:25:30 +0900

    不要箇所削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-  before_action :set_user_current, only: [:index]
   before_action :set_user_params, only: [:show]
 
   def index
@@ -13,10 +12,6 @@ class UsersController < ApplicationController
   end
 
   private
-  
-  def set_user_current
-    @user = User.find(current_user.id)
-  end
 
   def set_user_params
     @user = User.find(params[:id])
